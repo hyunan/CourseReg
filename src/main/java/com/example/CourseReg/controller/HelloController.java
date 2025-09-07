526e26b176845f2ba94aad6c851bf366e7f9d45a
hyunan <68716940+hyunan@users.noreply.github.com>
2025-09-07 15:42:51 -0700

    controller not added

--- src/main/java/com/example/CourseReg/controller/HelloController.java
+++ src/main/java/com/example/CourseReg/controller/HelloController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        System.out.println("Hello world!");
         return String.format("Hello %s!", name);
     }
 }
